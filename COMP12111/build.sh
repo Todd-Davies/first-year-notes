178c940d8491a224cb4fafcd69c604333381892f
Todd Davies <todd434@gmail.com>
2013-11-27 18:52:39 +0000

    Started 12111 notes

--- COMP12111/build.sh
+++ COMP12111/build.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
-
-ENGINE="latex"
-
-OPTS=`getopt -o n:e: --long name:,engine: -- "$@"`
-eval set -- "$OPTS"
-
-while true
-do
-  case "$1" in
-    -n|--name)
-      name="$2"
-      shift 2
-      ;;
-    -e|--engine)
-      engine="$2"
-      shift 2
-      ;;
-    --)
-      shift
-      break
-      ;;
-     *)
-      echo "Internal error!"
-      exit 1
-  esac
-done
-
-aspell -t check $name
-$engine $name
+aspell -t check notes.tex
+pdflatex three_box_model.tex
+pdflatex notes.tex
