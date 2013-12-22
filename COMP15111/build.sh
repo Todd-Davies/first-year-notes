000aaa0bcda4b7cd56ecdc1074044a3e2a552aea
Todd Davies <todd434@gmail.com>
2013-12-22 15:08:10 +0000

    Upgraded the project layout

--- COMP15111/build.sh
+++ COMP15111/build.sh
@@ -4,3 +4,6 @@ aspell -t check meta.tex
 pdflatex two_box_model_diagram.tex
 pdflatex notes.tex
 pdflatex kindle.tex
+# In case the Author field isn't set
+exiftool notes.pdf -Author="Todd Davies"
+
