438ca21e3aee762854e5898a085a20231e1d4416
Todd Davies <todd434@gmail.com>
2014-04-03 21:35:51 +0100

    Started writing about OBT's

---
+++ COMP16212/trees/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ..
+aspell -t check content.tex
+aspell -t check meta.tex
+directories=(trees);
+for dir in "${directories[@]%*/}"; do
+  cd $dir;
+  for i in `ls *.tex`; do
+    pdflatex $i;
+  done;
+  cd ..
+done;
+pdflatex notes.tex
+pdflatex kindle.tex
+# In case the Author field isn't set
+exiftool notes.pdf -Author="Todd Davies"
+
