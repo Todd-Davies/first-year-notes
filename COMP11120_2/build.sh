7e16d7b2536f49b15c36b1430a9242f14580b365
Todd Davies <todd434@gmail.com>
2014-04-11 21:02:42 +0100

    Started relations

--- COMP11120_2/build.sh
+++ COMP11120_2/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 aspell -t check content.tex
 aspell -t check meta.tex
+directories=(digraphs);
+for dir in "${directories[@]%*/}"; do
+  cd $dir;
+  for i in `ls *.tex`; do
+    pdflatex $i;
+  done;
+done;
+cd ..
 pdflatex notes.tex
 pdflatex kindle.tex
 # In case the Author field isn't set
