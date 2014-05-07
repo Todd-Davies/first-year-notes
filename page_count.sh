b53e6ce0dcb945423274ad7dbb467a6280cdfe59
Todd Davies <todd434@gmail.com>
2014-05-07 18:02:15 +0100

    Awesomified the page count script

--- page_count.sh
+++ page_count.sh
@@ -1,5 +1,5 @@
 total=0;
-for i in $(find ./ -name notes.pdf);
+for i in $(find ./ -name notes.pdf ; find ./*/flashcards/ -name doc.pdf );
   do
   nextCount=$(exiftool $i | awk "/Page Count/ {print}" | tail -c 3)
   total=$(echo $nextCount + $total | bc);
