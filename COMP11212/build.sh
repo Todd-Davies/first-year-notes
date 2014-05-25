8f028e429b5b7689d81e305d491396ce5403c83a
Todd Davies <todd434@gmail.com>
2014-05-25 19:07:48 +0100

    Did more 121 notes

--- COMP11212/build.sh
+++ COMP11212/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 aspell -t check content.tex
 aspell -t check meta.tex
-directories=(automata);
-for dir in "${directories[@]%*/}"; do
+directories=(parse_trees automata)
+for dir in "${directories[@]}"
+do
   cd $dir;
   for i in `ls *.tex`; do
     pdflatex $i;
   done;
+  cd ..
 done;
-cd ..
 pdflatex notes.tex
 pdflatex notes.tex
 pdflatex kindle.tex
